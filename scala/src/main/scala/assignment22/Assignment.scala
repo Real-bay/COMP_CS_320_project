cd6342fe440f7f503e6d0a435f98b1e17b203f55
Kasperi Kouri <kasperi.kouri@tuni.fi>
2022-12-09 01:19:18 +0200

    Refactor code to follow functional programming paradigm

--- scala/src/main/scala/assignment22/Assignment.scala
+++ scala/src/main/scala/assignment22/Assignment.scala
@@ -1,6 +1,6 @@
 package assignment22
 
-import org.apache.spark.ml.clustering.KMeans
+import org.apache.spark.ml.clustering.{KMeans, KMeansModel}
 import org.apache.spark.ml.evaluation.ClusteringEvaluator
 import org.apache.spark.ml.feature.{MinMaxScaler, VectorAssembler}
 import org.apache.spark.sql.DataFrame
@@ -34,135 +34,148 @@ class Assignment {
   val dataD2WithLabels: DataFrame = dataD2.withColumn("LABEL", labelToInteger(dataD2("LABEL")))
 
 
+  def getMin(df: DataFrame, colName: String): Double = {
+    df.select(colName).agg(Map(colName -> "min")).first.getDouble(0)
+  }
+
+  def getMax(df: DataFrame, colName: String): Double = {
+    df.select(colName).agg(Map(colName -> "max")).first.getDouble(0)
+  }
+
+  def getFeatureDf(df: DataFrame, featureCols: Array[String]): DataFrame = {
+    new VectorAssembler()
+      .setInputCols(featureCols)
+      .setOutputCol("features")
+      .transform(df)
+  }
+
+  def getScaledData(df: DataFrame, colNames: Array[String]): DataFrame = {
+    new MinMaxScaler()
+    .setInputCol("features")
+    .setOutputCol("scaledFeatures")
+    .fit(df)
+    .transform(df)
+      .drop(colNames: _*)
+      .withColumnRenamed("scaledFeatures", "features")
+  }
+
+  def getModel(df: DataFrame, k: Int): KMeansModel = {
+    new KMeans()
+      .setK(k)
+      .setSeed(1)
+      .fit(df)
+  }
+
+  def deNormalize(norm: Double, min: Double, max: Double): Double = {
+    norm * (max - min) + min
+  }
+
+
   def task1(df: DataFrame, k: Int): Array[(Double, Double)] = {
 
     // get min and max values for a and b in df
-    val minA = df.select("a").agg(Map("a" -> "min")).first.getDouble(0)
-    val maxA = df.select("a").agg(Map("a" -> "max")).first.getDouble(0)
-    val minB = df.select("b").agg(Map("b" -> "min")).first.getDouble(0)
-    val maxB = df.select("b").agg(Map("b" -> "max")).first.getDouble(0)
+    val minA = getMin(df, "a")
+    val maxA = getMax(df, "a")
+    val minB = getMin(df, "b")
+    val maxB = getMax(df, "b")
 
     // create new data frame with new column "features"
-    val cols = Array("a", "b")
-    val assembler = new VectorAssembler().setInputCols(cols).setOutputCol("features")
-    val featureDf = assembler.transform(df.drop("LABEL"))
+    val featureDf = getFeatureDf(df, Array("a", "b"))
 
     // normalize featureDF to [0, 1]
-    val scaler = new MinMaxScaler().setInputCol("features").setOutputCol("scaledFeatures")
-    val scalerModel = scaler.fit(featureDf)
-    val scaledData = scalerModel.transform(featureDf)
+    val scaledData = getScaledData(featureDf, Array("features", "LABEL"))
 
-    // create the k-means model
-    val kmeans = new KMeans().setK(k).setSeed(1).setFeaturesCol("scaledFeatures")
-    val model = kmeans.fit(scaledData)
-
-    // de-normalize the centers and return them
-    val centers = model.clusterCenters.map(x => (x(0) * (maxA - minA) + minA, x(1) * (maxB - minB) + minB))
-    centers
+    // create the k-means model,get the centers, de-normalize them and return them
+    getModel(scaledData, k)
+      .clusterCenters
+      .map(x => (deNormalize(x(0), minA, maxA), deNormalize(x(1), minB, maxB)))
   }
 
 
   def task2(df: DataFrame, k: Int): Array[(Double, Double, Double)] = {
 
     // get min and max values for a, b and c in df
-    val minA = df.select("a").agg(Map("a" -> "min")).first.getDouble(0)
-    val maxA = df.select("a").agg(Map("a" -> "max")).first.getDouble(0)
-    val minB = df.select("b").agg(Map("b" -> "min")).first.getDouble(0)
-    val maxB = df.select("b").agg(Map("b" -> "max")).first.getDouble(0)
-    val minC = df.select("c").agg(Map("c" -> "min")).first.getDouble(0)
-    val maxC = df.select("c").agg(Map("c" -> "max")).first.getDouble(0)
+    val minA = getMin(df, "a")
+    val maxA = getMax(df, "a")
+    val minB = getMin(df, "b")
+    val maxB = getMax(df, "b")
+    val minC = getMin(df, "c")
+    val maxC = getMax(df, "c")
 
     // create new data frame with new column "features"
-    val cols = Array("a", "b", "c")
-    val assembler = new VectorAssembler().setInputCols(cols).setOutputCol("features")
-    val featureDf = assembler.transform(df.drop("LABEL"))
+    val featureDf = getFeatureDf(df, Array("a", "b", "c"))
 
     // normalize featureDF to [0, 1]
-    val scaler = new MinMaxScaler().setInputCol("features").setOutputCol("scaledFeatures")
-    val scalerModel = scaler.fit(featureDf)
-    val scaledData = scalerModel.transform(featureDf)
+    val scaledData = getScaledData(featureDf, Array("features", "LABEL"))
 
-    // create the k-means model
-    val kmeans = new KMeans().setK(k).setSeed(1).setFeaturesCol("scaledFeatures")
-    val model = kmeans.fit(scaledData)
-
-
-    // de-normalize the centers and return them
-    val centers = model.clusterCenters.map(x => (x(0) * (maxA - minA) + minA, x(1) * (maxB - minB) + minB, x(2) * (maxC - minC) + minC))
-    centers
+    // create the k-means model,get the centers, de-normalize them and return them
+    getModel(scaledData, k)
+      .clusterCenters
+      .map(x => (deNormalize(x(0), minA, maxA), deNormalize(x(1), minB, maxB), deNormalize(x(2), minC, maxC)))
   }
+
 
   def task3(df: DataFrame, k: Int): Array[(Double, Double)] = {
 
     // get min and max values for a and b in df
-    val minA = df.select("a").agg(Map("a" -> "min")).first.getDouble(0)
-    val maxA = df.select("a").agg(Map("a" -> "max")).first.getDouble(0)
-    val minB = df.select("b").agg(Map("b" -> "min")).first.getDouble(0)
-    val maxB = df.select("b").agg(Map("b" -> "max")).first.getDouble(0)
+    val minA = getMin(df, "a")
+    val maxA = getMax(df, "a")
+    val minB = getMin(df, "b")
+    val maxB = getMax(df, "b")
 
     // create new data frame with new column "features"
-    val cols = Array("a", "b")
-    val assembler = new VectorAssembler().setInputCols(cols).setOutputCol("features")
-    val featureDf = assembler.transform(df)
+    val featureDf = getFeatureDf(df, Array("a", "b"))
 
     // normalize featureDF to [0, 1]
-    val scaler = new MinMaxScaler().setInputCol("features").setOutputCol("scaledFeatures")
-    val scalerModel = scaler.fit(featureDf)
-    val scaledData = scalerModel.transform(featureDf)
+    val scaledData = getScaledData(featureDf, Array("features"))
 
     // create the k-means model
-    val kmeans = new KMeans().setK(k).setSeed(1).setFeaturesCol("scaledFeatures")
-    val model = kmeans.fit(scaledData)
+    val model = getModel(scaledData, k)
 
-    // Make predictions
-    val predictions = model.transform(scaledData)
-
-    // Select two clusters with the highest number of fatal cases
-    val fatalClusters = predictions.filter(predictions("LABEL") === 0).groupBy("prediction").count().orderBy(col("count").desc).take(2).map(x => x(0).asInstanceOf[Int])
+    // Make predictions and select two clusters with the highest number of fatal cases
+    val fatalClusters = model
+      .transform(scaledData)
+      .filter(col("LABEL") === 0)
+      .groupBy("prediction")
+      .count()
+      .orderBy(col("count").desc)
+      .take(2)
+      .map(x => x(0).asInstanceOf[Int])
 
     // de-normalize the centers and return the correct ones
-    val centers = model.clusterCenters.map(x => (x(0) * (maxA - minA) + minA, x(1) * (maxB - minB) + minB))
-    Array(centers(fatalClusters(0)), centers(fatalClusters(1)))
+    Array(model.clusterCenters(fatalClusters(0)), model.clusterCenters(fatalClusters(1)))
+      .map(x => (deNormalize(x(0), minA, maxA), deNormalize(x(1), minB, maxB)))
   }
+
+
+  def getSilhouetteScore(df: DataFrame, k: Int, max: Int): Array[(Int, Double)] = {
+
+    if (k > max) {
+      return Array[(Int, Double)]()
+    }
+
+    // create the k-means model and make predictions
+    val predictions = getModel(df, k)
+      .transform(df)
+
+    // Evaluate clustering by computing Silhouette score
+    val score = new ClusteringEvaluator().evaluate(predictions)
+
+    // add score to scores array
+    Array.concat(Array((k, score)), getSilhouetteScore(df, k + 1, max))
+  }
+
 
   // Parameter low is the lowest k and high is the highest one.
   def task4(df: DataFrame, low: Int, high: Int): Array[(Int, Double)]  = {
 
-    var scores = Array[(Int, Double)]()
-
     // create new data frame with new column "features"
-    val cols = Array("a", "b")
-    val assembler = new VectorAssembler().setInputCols(cols).setOutputCol("features")
-    val featureDf = assembler.transform(df.drop("LABEL"))
+    val featureDf = getFeatureDf(df, Array("a", "b"))
 
     // normalize featureDF to [0, 1]
-    val scaler = new MinMaxScaler().setInputCol("features").setOutputCol("scaledFeatures")
-    val scalerModel = scaler.fit(featureDf)
-    val scaledData = scalerModel.transform(featureDf).drop("features", "LABEL").withColumnRenamed("scaledFeatures", "features")
-    scaledData.show()
+    val scaledData = getScaledData(featureDf, Array("features", "LABEL"))
 
-    // Do this with recursion later!!!
-    for (k <- low to high) {
-
-      // create the k-means model
-      val kmeans = new KMeans().setK(k).setSeed(1)
-      val model = kmeans.fit(scaledData)
-
-      scaledData.show()
-
-
-      // Make predictions
-      val predictions = model.transform(scaledData)
-
-      // Evaluate clustering by computing Silhouette score
-      val evaluator = new ClusteringEvaluator()
-      val score = evaluator.evaluate(predictions)
-
-      // add score to scores array
-      scores = scores :+ (k, score)
-    }
-
-    scores
+    getSilhouetteScore(scaledData, low, high)
   }
 
 }
